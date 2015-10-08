f965abc8ecc29d8115e58c6ab7e9a1c3a71f639f
Deshi-basara <simon@i-was-perfect.net>
2015-10-08 22:19:29 +0000

    Setup updated to latest release

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from distutils.core import setup
 setup(
-    name='coovie2',
-    packages=['coovie2'],
-    version='0.0.1',
-    desciption='List movies with their IMDb-Rating',
-    author='Deshi Basara',
-    author_email='simon@i-was-perfect.net',
-    url='https://github.com/deshi-basara/coovie2'
+    name="coovie2",
+    packages=["coovie2"],
+    version="0.0.1",
+    desciption="Cli tool for listing movies inside a path according to their" +
+               "IMDb-rating",
+    author="Deshi Basara",
+    author_email="dorschbert@googlemail.com",
+    url="https://github.com/deshi-basara/coovie2",
+    install_requires=[
+        "Click",
+        "termcolor",
+        "imdbpie",
+        "requests",
+        "BeautifulSoup"
+    ],
+    entry_points='''
+        [console_scripts]
+        coovie2=coovie2:main
+    '''
 )
