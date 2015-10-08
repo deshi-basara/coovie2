caeabf7079ce8d2cc812160e8fcc32235b2d910f
Deshi-basara <simon@i-was-perfect.net>
2015-10-08 12:59:06 +0000

    Startpage scraper prototype

--- coovie2/movie.py
+++ coovie2/movie.py
@@ -1,19 +1,20 @@
 from imdbpie import Imdb
+import requests
 imdb = Imdb(cache=True)
 
 
 class Movie(object):
-
     def __init__(self, title, year, path, folder):
         self.title = title
         self.original_title = ""
         self.year = year
         self.path = path
         self.folder = folder
-        self.rating = -1
-        self.imdb_top = -1
+        self.rating = 0
+        self.imdb_top = False
 
     def fetch_rating(self):
+        print("Fetching: " + self.title)
         search_results = imdb.search_for_title(self.title)
 
         # try to get the result by year
@@ -24,6 +25,24 @@ class Movie(object):
                 self.original_title = title_result.title
                 self.rating = title_result.rating
                 break
+
+        # no imdb entry found for title, try to find imdb-entry via search-
+        # engines
+        search_engine = "https://startpage.com/do/metasearch.pl"
+        search_payload = {
+            "hmb": "1",
+            "cat": "web",
+            "cmd": "process_search",
+            "language": "deutsch",
+            "engine0": "v1all",
+            "abp": "-1",
+            "nj": "0",
+            "query": self.title + " " + self.year + " imdb",
+            "pg": "0"
+        }
+        search_request = requests.post(search_engine, search_payload)
+        print(search_request.text)
+        print(search_request.content)
 
     def print_data(self, longest_title):
         data_str = "{rating} | {title} {year: <0{padding}} | {path}".format(
